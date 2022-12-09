7d0503b8165c85dfb70bd9ffe3df671116e1352f
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2022-12-09 14:04:12 -0500

    Clean code and formatting

--- frontend/static/js/dropdown_functions.js
+++ frontend/static/js/dropdown_functions.js
@@ -6,34 +6,38 @@ function disabled_dropdown(event) {
     let tooltip_id_list = $.map($('.custom-tooltip > span'), span_id => span_id.id);
 
     console.log("before condition",event.target.id)
-    // console.log(tooltip_id_list)
 
-    
+
     if (event.target.id == "Reset-Icon")
     {
         for(let i = 0; i < tooltip_id_list.length; i++)
         {
         let split_id = tooltip_id_list[i].split("Tooltip")[0]
         console.log("split", split_id)
-        // console.log(split_id)
+
         let button_id = split_id + "Icon-Button"
         let tooltip_id = split_id + "Tooltip"
         let icon_id = split_id + "Icon"
-        let tooltip_text = split_id.replaceAll("-", " ")
+
+        //Note Get get of last character which is "-". Then replace the middle "-" with a space
+        let tooltip_text = split_id.slice(0, -1).replaceAll("-", " ")
+        
+        //Note Makes button clickable
         $(`#${button_id}`).prop("disabled", false);
 
-        //Note Changes tooltiptext to disabled
+        //Note Changes tooltiptext to it's original text
         $(`#${tooltip_id}`).text(tooltip_text)
         
-        //Note Changes icon color to red
+        //Note Changes icon color to it's original color
         $(`#${icon_id}`).css("color", "#004489")
         }
 
     }
+    
     //Note "Type-Address" won't disable other icons
     else if ((event.target.id != "Reset-Icon") && (event.target.id != "Type-Address"))
     {
-    console.log("342")
+    
 
     //Note Passing tooltip id because it's the easiest to split
     for(let i = 0; i < tooltip_id_list.length; i++)
@@ -52,38 +56,23 @@ function disabled_dropdown(event) {
     // console.log("conditional", conditional_id)
     // console.log(event.target.id)
 
+    //Note Harded coding that the "Website" and "Reset" icon don't get disabled
+    if ((conditional_id != "Website") && (conditional_id != "Reset"))
+    {
+    //Note Makes button unclickable
+    $(`#${button_id}`).prop("disabled", true);
 
-        //Organization Other condtion ------
-        //Note Harded coding that the "Website" and "Reset" icon don't get disabled
-        if ((event.target.id != conditional_id) && (conditional_id != "Website") && (conditional_id != "Reset"))
-        {
-        //Note Makes button unclickable
-        $(`#${button_id}`).prop("disabled", true);
-
-        //Note Changes tooltiptext to disabled
-        $(`#${tooltip_id}`).text("Disabled")
-        
-        //Note Changes icon color to red
-        $(`#${icon_id}`).css("color", "red")
-        }
+    //Note Changes tooltiptext to disabled
+    $(`#${tooltip_id}`).text("Disabled")
+    
+    //Note Changes icon color to red
+    $(`#${icon_id}`).css("color", "red")
+    }
         
     }   
     }
   }
-function reset_dropdown(){
-            //FIXME Testing the Reset
-            if ((conditional_id == "Reset") || (conditional_id == "Type-Address"))
-            {
-            //Note Makes button unclickable
-            $(`#${button_id}`).prop("disabled", false);
-    
-            //Note Changes tooltiptext to disabled
-            $(`#${tooltip_id}`).text(split_id)
-            
-            //Note Changes icon color to red
-            $(`#${icon_id}`).css("color", "#004489")
-            }
-}
+
 function initilize_dictionary(event)
 {
     let string_url = window.location.href
