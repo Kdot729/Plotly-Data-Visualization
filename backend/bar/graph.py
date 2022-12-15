467fccd47d0b5143ae5c7237ca1d1a71e7a3452f
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2022-12-15 13:23:23 -0500

    Fixed coloring of bar
    When changing "Type" to "Buy", bar chart is green. When it's "Sell", bar chart is red.

--- backend/bar/graph.py
+++ backend/bar/graph.py
@@ -3,21 +3,20 @@ import plotly.express as plotlyX
 
 
 def create_count_transactions_graph(DataFrame, type):
-
+        print(DataFrame)
         if type == "Total":
                 y_axis = ["Sell", "Buy"]
-                color = {'Sell': 'red', 'Buy': 'green'}
+                color = {'Sell': 'red','Buy': 'green'}
+                figure = multi_color_bar(DataFrame, y_axis, color)
         elif type == "Buy":
                 y_axis = "Buy"
-                color = {'Buy': 'green'}
+                color = "green"
+                figure = single_color_bar(DataFrame, y_axis, color)
         elif type == "Sell":
                 y_axis = "Sell"
-                color = {'Sell': 'red'}
-        figure = plotlyX.bar(DataFrame, 
-                        x="Address", 
-                        y=y_axis,
-                        color_discrete_map=color,
-                        title="Transactions by Address")
+                color = "red"
+                figure = single_color_bar(DataFrame, y_axis, color)
+
                         
         figure.update_xaxes(tickfont_size=7)
         figure.update_yaxes(title="Amount of Transactions")
@@ -38,4 +37,19 @@ def create_count_transactions_graph(DataFrame, type):
                 "dtick": "1",
                 }
         )
+        return figure
+
+def multi_color_bar(DataFrame, y_axis, color):
+        return plotlyX.bar(DataFrame, 
+                        x="Address", 
+                        y=y_axis,
+                        color_discrete_map=color,
+                        title="Transactions by Address")
+
+def single_color_bar(DataFrame, y_axis, color):
+        figure = plotlyX.bar(DataFrame, 
+        x="Address", 
+        y=y_axis,
+        title="Transactions by Address")
+        figure.update_traces(marker_color=color)
         return figure
