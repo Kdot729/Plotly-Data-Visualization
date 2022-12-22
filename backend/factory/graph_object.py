2e9bf492dc922ef888b4371602e8768221ff399e
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2022-12-22 13:14:23 -0500

    Created a new dynamic function for creating the first page

--- backend/factory/graph_object.py
+++ backend/factory/graph_object.py
@@ -19,7 +19,13 @@ class Graph_Factory(ABC):
         elif specificity == "count_transactions":
             return Count_Bar_Graph()
 
-
+    @abstractmethod
+    def find_filepath(self):
+        pass
+    
+    @abstractmethod
+    def hardcode_column_dictionary(self):
+        pass
 
     def create_DataFrame(self, tool):
         return general_functions.create_basic_DataFrame(tool)
@@ -40,6 +46,13 @@ class Graph_Factory(ABC):
     def create_plotly(self, graph_type):
         pass
 
+    @abstractmethod
+    def get_template(self):
+        pass
+
+    @abstractmethod
+    def hardcode_create_plotly(self):
+        pass
     
     def create_badges(self):
         self.badges = general_functions.create_badges(self.columns_name["Address Column"], self.DataFrame)
@@ -66,6 +79,12 @@ class Count_Bar_Graph(Graph_Factory):
     def __init__(self):
         super().__init__()
 
+    def find_filepath(self):
+        self.filepath = f"graph/bar/count_transactions.html"
+
+    def hardcode_column_dictionary(self):
+        self.columns_name = {"Address Column": "Address", "Inequality Column": "Total"}
+        
     def create_DataFrame(self, tool):
         self.DataFrame = super().create_DataFrame(tool)
         self.DataFrame = dataframe.create_count_transactions_bar_DataFrame(self.DataFrame)
@@ -82,15 +101,30 @@ class Count_Bar_Graph(Graph_Factory):
                 self.DataFrame = self.DataFrame.filter(["Address", type_column_name])
                 self.DataFrame = self.DataFrame[(self.DataFrame[list(self.DataFrame.columns)] != 0).all(axis=1)]
 
+    def hardcode_create_plotly(self):
+        self.plotly_graph = bar_graph.create_count_transactions_graph(self.DataFrame, "Total")
+
     def create_plotly(self, graph_type):
         self.plotly_graph = bar_graph.create_count_transactions_graph(self.DataFrame, graph_type)
 
-
+    def get_template(self):
+        self.inequality_dictionary = general_functions.sort_Inequality_List(self.DataFrame, self.columns_name["Inequality Column"])
+        return render_template(template_name_or_list = self.filepath,
+                            graphJSON=self.graphJSON, 
+                            address_list=self.address_list,
+                            inequality_dictionary=self.inequality_dictionary,
+                            badges = self.badges)
 class Basic_Scatter_Graph(Graph_Factory):
 
     def __init__(self):
         super().__init__()
-    
+
+    def find_filepath(self):
+        self.filepath = f"graph/scatter/basic.html"
+
+    def hardcode_column_dictionary(self):
+        self.columns_name = {"Address Column": "Buyer", "Inequality Column": "ETH"}
+
     def create_DataFrame(self, tool):
         self.DataFrame = super().create_DataFrame(tool)
         #Note unmodifed_DataFrame is the original DataFrame which is going to be used to get the address_list
@@ -103,9 +137,20 @@ class Basic_Scatter_Graph(Graph_Factory):
     def filter_columns_DataFrame(self, type_column_name):
         self.DataFrame = self.DataFrame.filter(["Date", "Hash", "ETH", type_column_name])
 
+    def hardcode_create_plotly(self):
+        self.plotly_graph = scatter_graph.create_scatter_graph(self.DataFrame, "Buyer")
 
     def create_plotly(self, graph_type):
         self.plotly_graph = scatter_graph.create_scatter_graph(self.DataFrame, graph_type)
 
+    def get_template(self):
+        self.inequality_dictionary = general_functions.sort_Inequality_List(self.DataFrame, self.columns_name["Inequality Column"])
+        return render_template(template_name_or_list = self.filepath,
+                            graphJSON=self.graphJSON, 
+                            address_list=self.address_list,
+                            inequality_dictionary=self.inequality_dictionary,
+                            date_dictionary={"Min Date": '2021-10-08',
+                   "Max Date": date.today() },
+                            badges = self.badges)
 
         
--- backend/page.py
+++ backend/page.py
@@ -1,5 +1,16 @@
 from backend.factory.graph_object import Graph_Factory
 
+def initial_page_render(specificity, tool):
+    graph = Graph_Factory.build_graph(specificity)
+    graph.find_filepath()
+    graph.create_DataFrame(tool)
+    graph.hardcode_column_dictionary()
+    graph.hardcode_create_plotly()
+    graph.create_badges()
+    graph.create_address_list()
+    graph.convert_JSON()
+    return graph.get_template()
+
 def page_rerender(frontend_dictionary):
     graph = Graph_Factory.build_graph(frontend_dictionary['Specificity'])
     graph.create_DataFrame(frontend_dictionary["Tool"])
@@ -14,6 +25,5 @@ def page_rerender(frontend_dictionary):
     graph.create_address_list()
     graph.convert_JSON()
 
-    # print(graph.send_to_frontend(frontend_dictionary["ID of Dropdown"]))
     return graph.send_to_frontend(frontend_dictionary["ID of Dropdown"])
 
