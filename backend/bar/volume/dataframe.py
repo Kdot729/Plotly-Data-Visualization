915d59f122a0add6fecb7559bf9739333ae088fc
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2022-12-31 13:23:42 -0500

    Imported code
    Imported new code for volume graph

--- backend/bar/volume/dataframe.py
+++ backend/bar/volume/dataframe.py
@@ -15,11 +15,9 @@ def create_volumne_DataFrame(DataFrame):
         #Note Make "Date" a column
         DataFrame = DataFrame.reset_index(level=0)
 
-
-        day_list = []
-        month_list = []
-        year_list = []
+        day_of_week_name_list = []
         year_and_month_list = []
+
         for i in range(0, len(DataFrame)):
                 split_date = DataFrame["Date"][i][:10]
 
@@ -32,14 +30,17 @@ def create_volumne_DataFrame(DataFrame):
                 #Note Get full year
                 year = datetime.datetime.strptime(split_date, '%Y-%m-%d').strftime('%Y')
 
-                day_list.append(day_of_week)
-                month_list.append(month)
-                year_list.append(year)
+                day_of_week_name_list.append(day_of_week)
                 year_and_month_list.append(f"{month} {year}" )
 
-        DataFrame.insert(2, "Year", year_list, True)
-        DataFrame.insert(3, "Month", month_list, True)
-        DataFrame.insert(4, "Day", day_list, True)
-        DataFrame.insert(5, "Month Year", year_and_month_list, True)
+        #Note Insert the lists as columns
+        DataFrame.insert(2, "Day", day_of_week_name_list, True)
+        DataFrame.insert(3, "Month Year", year_and_month_list, True)
+
+        #Note Sort false will preserve the order which is acendening to the current date
+        DataFrame = DataFrame.groupby(["Day", "Month Year"], sort=False).sum('ETH')
+
+        #Note Make the index as columns
+        DataFrame = DataFrame.reset_index()
 
         return DataFrame
--- backend/bar/volume/graph.py
+++ backend/bar/volume/graph.py
@@ -2,11 +2,22 @@ import plotly.express as plotly
 
 def create_volume_graph(DataFrame):
 
+    #Note Need to convert "Month Year" to a list to pass into "category_orders" because the y-axis is bugged. For some reason "February 2022" doesn't appear after "January 2022"
+    #Note Reverse the list so the start of trading is at the bottom
+    y_axis = (DataFrame["Month Year"].tolist())[::-1]
+
     figure = plotly.bar(DataFrame, 
-                        x="ETH", 
-                        y="Month Year", 
-                        color='Day', 
-                        orientation='h')
+                    x="ETH", 
+                    y="Month Year", 
+                    color='Day', 
+                    orientation='h',
+                    category_orders={
+                            #Note Reorder the horizontal bars so "Monday" is first and "Sunday" is last
+                            "Day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+
+                            #Note This is to fix the y-axis because "February" is bugged
+                            "Month Year": y_axis
+                            })
 
     #Note Create title for graph and center it
     figure.update_layout(title_text="Volume", title_x=0.5)
@@ -25,3 +36,4 @@ def create_volume_graph(DataFrame):
                     })
 
     return figure
+
