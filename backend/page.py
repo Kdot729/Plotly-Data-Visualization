af7f31a7bdf834456acec59ec328136b57e3e788
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-20 17:04:20 -0400

    Changed function parameters

--- backend/page.py
+++ backend/page.py
@@ -1,9 +1,9 @@
 from backend.factory.superclass import Graph_Factory
 
-def Render_Graph(graph, specificity, tool):
+def Render_Graph(Graph_Name, Tool):
     
     #Note Graph_Factory is abstract so you can't call Graph_Factory() by itself thats why it's chained with build_graph
-    graph = Graph_Factory.build_graph(graph, specificity, tool)
+    graph = Graph_Factory.build_graph(Graph_Name, Tool)
     graph.create_DataFrame()
     graph.Create_Plotly()
     graph.Convert_Plotly_to_JSON()
