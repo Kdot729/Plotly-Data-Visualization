b95a5b1f6c5d0e0ff1bf35501d35626d4806f884
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-20 13:45:16 -0400

    Removed function to render page
    Website is static so we don't need to rerender the page

--- backend/page.py
+++ backend/page.py
@@ -12,21 +12,3 @@ def initial_page_render(graph, specificity, tool):
     graph.convert_JSON()
     graph.create_inequality_dictionary()
     return graph.initialize_template()
-
-def page_rerender(frontend_dictionary):
-    print("len", len(frontend_dictionary))
-    graph = Graph_Factory.build_graph(frontend_dictionary["Graph"], frontend_dictionary['Specificity'], frontend_dictionary["Tool"])
-    graph.create_DataFrame()
-    graph.create_column_dictionary(frontend_dictionary["Type"])
-    graph.filter_columns_DataFrame()
-    graph.sort_DataFrame(frontend_dictionary)
-
-    graph.create_plotly()
-
-    graph.create_badges()
-
-    graph.create_address_list()
-    graph.convert_JSON()
-    graph.create_inequality_dictionary()
-    return graph.rerender_template(frontend_dictionary["ID of Dropdown"])
-
