4c918378c9d1f44f76531426991d533fa8dc531d
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-20 13:45:03 -0400

    Removed route
    Website is static so we don't need to rerender the page

--- app.py
+++ app.py
@@ -2,18 +2,10 @@ from flask import Flask, request
 import backend.page as page
 app = Flask(__name__,  static_url_path='', static_folder="frontend/static", template_folder="frontend/templates")
 
-
 @app.route('/graph/<string:graph>/<string:specificity>/<string:tool>')
 def page_router(graph, specificity, tool):
     print(graph, specificity, tool)
     return page.initial_page_render(graph, specificity, tool)
-
-@app.route('/update_page_router', methods=['GET'])
-def update_page_router(): 
-
-    #Note Convert request.args. An ImmutableMultiDict to a regular dictionary
-    frontend_dictionary = dict(request.args)
-    return page.page_rerender(frontend_dictionary)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
