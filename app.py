b32f198e30a4b661bacbd3bc3c556e06fc3c07f0
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-20 17:12:10 -0400

    Removed route
    Don't need because we're using Route_Graph

--- app.py
+++ app.py
@@ -7,14 +7,9 @@ app = Flask(__name__,  static_url_path='', static_folder="frontend/static", temp
 def Home(): 
     return render_template(template_name_or_list="home.html")
 
-@app.route('/graph/<string:graph>/<string:specificity>/<string:tool>')
-def page_router(graph, specificity, tool):
-    return page.Render_Graph(graph, specificity, tool)
-
 @app.route('/graph/<string:Graph_Name>/<string:Tool>')
 def Route_Graph(Graph_Name, Tool):
     return page.Render_Graph(Graph_Name, Tool)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
