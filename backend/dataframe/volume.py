d2817113ff3a8373d17bb5b4cb06965c63a7d459
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-21 18:17:13 -0400

    Deleted code to create date lists and insert them
    There's functions in transaction to do that

--- backend/dataframe/volume.py
+++ backend/dataframe/volume.py
@@ -1,28 +1,4 @@
-import pandas as panda
-import datetime
-
 def Create_Volume_Dataframe(DataFrame):
-
-        day_of_week_name_list = []
-        year_and_month_list = []
-
-        for Current_Date in DataFrame["Date"].tolist():
-
-                #Note Get full weekday name 
-                day_of_week= datetime.datetime.strptime(Current_Date, '%Y-%m-%d').strftime('%A')
-
-                #Note Get abbreviated month name
-                month = datetime.datetime.strptime(Current_Date, '%Y-%m-%d').strftime('%b')
-
-                #Note Get full year
-                year = datetime.datetime.strptime(Current_Date, '%Y-%m-%d').strftime('%Y')
-
-                day_of_week_name_list.append(day_of_week)
-                year_and_month_list.append(f"{month} {year}" )
-
-        #Note Insert the lists as columns
-        DataFrame.insert(2, "Day", day_of_week_name_list, True)
-        DataFrame.insert(3, "Month Year", year_and_month_list, True)
 
         #Note Sort false will preserve the order which is acendening to the current date
         DataFrame = DataFrame.groupby(["Day", "Month Year"], sort=False).sum('ETH')
