f0206ab0c6088378b9f6066118ff0e51ff30a884
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-21 12:54:12 -0400

    Changed variable name

--- backend/page.py
+++ backend/page.py
@@ -2,9 +2,8 @@ from backend.factory.superclass import Graph_Factory
 
 def Render_Graph(Graph_Name, Tool):
     
-    #Note Graph_Factory is abstract so you can't call Graph_Factory() by itself thats why it's chained with build_graph
-    graph = Graph_Factory.build_graph(Graph_Name, Tool)
-    graph.create_DataFrame()
-    graph.Create_Plotly()
-    graph.Convert_Plotly_to_JSON()
-    return graph.Render_Graph()
+    Chosen_Graph = Graph_Factory.build_graph(Graph_Name, Tool)
+    Chosen_Graph.create_DataFrame()
+    Chosen_Graph.Create_Plotly()
+    Chosen_Graph.Convert_Plotly_to_JSON()
+    return Chosen_Graph.Render_Graph()
