6da36d21543715e7268de99300f77522d2c6a5d8
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-22 11:40:41 -0400

    Created function to loop and insert row

--- backend/dataframe/sunburst.py
+++ backend/dataframe/sunburst.py
@@ -1,30 +1,58 @@
 import pandas as panda
 import datetime
 
+
+def Insert_Into_Row(Graph_Dataframe, Looped_Dataframe, Replaced_Value):
+
+    # Graph_Dataframe = panda.DataFrame(columns=['id', 'parent', 'value'])
+    # Dataframe_Index = len(Graph_Dataframe.index)
+
+    for index, row in Looped_Dataframe.iterrows():
+        ETH_Column = row["ETH"]
+
+        if Replaced_Value == "Year":
+            Replace_Row = [row[Replaced_Value], "Total", ETH_Column]
+        elif Replaced_Value == "Month Year":
+            Replace_Row = [row[Replaced_Value], row[Replaced_Value][:4], ETH_Column]
+        elif Replaced_Value == "Date":
+            Replace_Row = [row[Replaced_Value], row[Replaced_Value][:7], ETH_Column]
+
+        Dataframe_Index = len(Graph_Dataframe.index)
+        Graph_Dataframe.loc[Dataframe_Index] = Replace_Row
+    
+    return Graph_Dataframe
+
 def create_sunburst_DataFrame(Year_Dataframe, Month_Year_Dataframe, Dataframe):
 
-    graph_DataFrame = panda.DataFrame(columns=['id', 'parent', 'value'])
+    Graph_Dataframe = panda.DataFrame(columns=['id', 'parent', 'value'])
+    Dataframe_Index = len(Graph_Dataframe.index)
 
-    for index, row in Year_Dataframe.iterrows():
-        graph_DataFrame.loc[len(graph_DataFrame.index)] = [row["Year"], "Total" ,row["ETH"]]
+    # for index, row in Year_Dataframe.iterrows():
+    #     Graph_Dataframe.loc[len(Graph_Dataframe.index)] = [row["Year"], "Total", row["ETH"]]
 
-    for index, row in Month_Year_Dataframe.iterrows():
-        graph_DataFrame.loc[len(graph_DataFrame.index)] = [row["Month Year"], row["Month Year"][:4] ,row["ETH"]]
+    # for index, row in Month_Year_Dataframe.iterrows():
+    #     Graph_Dataframe.loc[len(Graph_Dataframe.index)] = [row["Month Year"], row["Month Year"][:4], row["ETH"]]
 
-    for index, row in Dataframe.iterrows():
-        graph_DataFrame.loc[len(graph_DataFrame.index)] = [row["Date"], row["Date"][:7] ,row["ETH"]]
+    # for index, row in Dataframe.iterrows():
+    #     Graph_Dataframe.loc[len(Graph_Dataframe.index)] = [row["Date"], row["Date"][:7], row["ETH"]]
 
+    # print(graph_DataFrame.head())
+    Graph_Dataframe = Insert_Into_Row(Graph_Dataframe, Year_Dataframe, "Year")
+    Graph_Dataframe = Insert_Into_Row(Graph_Dataframe, Month_Year_Dataframe, "Month Year")
+    Graph_Dataframe = Insert_Into_Row(Graph_Dataframe, Dataframe, "Date")
+    
     override_text = []
 
     substring_checker = "0"
 
-    for index in graph_DataFrame['id']:
+    for index in Graph_Dataframe['id']:
 
             #Note If len 4 then it's the year
             if index == "Total":
                 value == "Total"
             elif len(index) == 4:
                 value = index
+
 
             #Note If len 7 then it's the year and month
             elif len(index) == 7:
@@ -42,6 +70,6 @@ def create_sunburst_DataFrame(Year_Dataframe, Month_Year_Dataframe, Dataframe):
             override_text.append(value)
 
     #Note Created a new column to be the text
-    graph_DataFrame["Text"] = override_text
+    Graph_Dataframe["Text"] = override_text
 
-    return graph_DataFrame
+    return Graph_Dataframe
