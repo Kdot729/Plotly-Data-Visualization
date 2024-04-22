12dcc173333b6d2a453fb6db6495c6160c09f12a
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-22 14:05:26 -0400

    Deleted loop to create the "Text"
    Second loop is reductant. Can do everything in the first loop

--- backend/dataframe/sunburst.py
+++ backend/dataframe/sunburst.py
@@ -40,26 +40,4 @@ def create_sunburst_DataFrame(Year_Dataframe, Month_Year_Dataframe, Dataframe):
             Dataframe_Index = len(Graph_Dataframe.index)
             Graph_Dataframe.loc[Dataframe_Index] = Insert_Row
 
-    # override_text = []
-
-    # for ID_Value in Graph_Dataframe['id']:
-
-    #     #Note If len 4 then it's the year
-    #     if len(ID_Value) == 4:
-    #         value = ID_Value
-
-    #     #Note If len 7 then it's the year and month
-    #     elif len(ID_Value) == 7:
-    #         month = datetime.datetime.strptime(ID_Value, '%Y-%m').strftime('%b')
-    #         value = month
-
-    #     elif len(ID_Value) == 10:
-    #         nth_day = datetime.datetime.strptime(ID_Value, '%Y-%m-%d').strftime('%-d')
-    #         value = f"{nth_day}th"
-
-    #     override_text.append(value)
-
-    # #Note Created a new column to be the text
-    # Graph_Dataframe["Text"] = override_text
-    print(Graph_Dataframe[:50])
     return Graph_Dataframe
