b078fd8139950a022850b39f4231ba847fecd430
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-22 14:04:15 -0400

    Moved the text creation code

--- backend/dataframe/sunburst.py
+++ backend/dataframe/sunburst.py
@@ -1,9 +1,9 @@
 import pandas as panda
-import datetime
+from datetime import datetime 
 
 def create_sunburst_DataFrame(Year_Dataframe, Month_Year_Dataframe, Dataframe):
 
-    Graph_Dataframe = panda.DataFrame(columns=['id', 'parent', 'value'])
+    Graph_Dataframe = panda.DataFrame(columns=['id', 'parent', 'value', "Text"])
 
     Dataframe_Array =   [
                             {"Dataframe": Year_Dataframe, "Column": "Year"},
@@ -21,37 +21,45 @@ def create_sunburst_DataFrame(Year_Dataframe, Month_Year_Dataframe, Dataframe):
             Value = Row[Dataframe_Column]
 
             if Dataframe_Column == "Year":
-                Insert_Row = [Value, "Total", ETH_Value]
+
+                Insert_Row = [Value, "Total", ETH_Value, Value]
+
             elif Dataframe_Column == "Month Year":
+
+                Abbreviated_Month = datetime.strptime(Value, '%Y-%m').strftime('%b')
                 #Note Slicing "Month Year" value
-                Insert_Row = [Value, Value[:4], ETH_Value]
-            elif Dataframe_Column == "Date":
+                Insert_Row = [Value, Value[:4], ETH_Value, Abbreviated_Month]
+
+            elif Dataframe_Column == "Date":    
+
+                Non_Zero_Padded_Day = datetime.strptime(Value, '%Y-%m-%d').strftime('%-d')
+                Nth_Day = f"{Non_Zero_Padded_Day}th"
                 #Note Slicing "Date" value
-                Insert_Row = [Value, Value[:7], ETH_Value]
+                Insert_Row = [Value, Value[:7], ETH_Value, Nth_Day]
 
             Dataframe_Index = len(Graph_Dataframe.index)
             Graph_Dataframe.loc[Dataframe_Index] = Insert_Row
 
-    override_text = []
+    # override_text = []
 
-    for ID_Value in Graph_Dataframe['id']:
+    # for ID_Value in Graph_Dataframe['id']:
 
-        #Note If len 4 then it's the year
-        if len(ID_Value) == 4:
-            value = ID_Value
+    #     #Note If len 4 then it's the year
+    #     if len(ID_Value) == 4:
+    #         value = ID_Value
 
-        #Note If len 7 then it's the year and month
-        elif len(ID_Value) == 7:
-            month = datetime.datetime.strptime(ID_Value, '%Y-%m').strftime('%b')
-            value = month
+    #     #Note If len 7 then it's the year and month
+    #     elif len(ID_Value) == 7:
+    #         month = datetime.datetime.strptime(ID_Value, '%Y-%m').strftime('%b')
+    #         value = month
 
-        elif len(ID_Value) == 10:
-            nth_day = datetime.datetime.strptime(ID_Value, '%Y-%m-%d').strftime('%-d')
-            value = f"{nth_day}th"
+    #     elif len(ID_Value) == 10:
+    #         nth_day = datetime.datetime.strptime(ID_Value, '%Y-%m-%d').strftime('%-d')
+    #         value = f"{nth_day}th"
 
-        override_text.append(value)
+    #     override_text.append(value)
 
-    #Note Created a new column to be the text
-    Graph_Dataframe["Text"] = override_text
-
+    # #Note Created a new column to be the text
+    # Graph_Dataframe["Text"] = override_text
+    print(Graph_Dataframe[:50])
     return Graph_Dataframe
