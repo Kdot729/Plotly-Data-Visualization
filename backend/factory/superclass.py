bd60c5994ce61db68a936fd148b437e3903bb125
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-23 16:35:34 -0400

    Assigned date string to a variable

--- backend/factory/superclass.py
+++ backend/factory/superclass.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 class Graph_Factory(ABC):
 
+    Date_Column = "Date"
+
     def __init__(self, Graph_Name, Tool):
         self.Graph_Name = Graph_Name
         self.Tool = Tool
@@ -36,21 +38,21 @@ class Graph_Factory(ABC):
         return render_template(template_name_or_list="graph.html", graphJSON=self.graphJSON, tool=self.Tool)
 
     def create_DataFrame(self):
-        self.Dataframe = panda.read_csv(f"csv/updated_{self.Tool}_transactions.csv", names=('Date', 'Hash', 'ETH', 'Seller', 'Buyer')) 
+        self.Dataframe = panda.read_csv(f"csv/updated_{self.Tool}_transactions.csv", names=(self.Date_Column, 'Hash', 'ETH', 'Seller', 'Buyer')) 
     
     def Strftime(self, Date, Format_Date):
         return datetime.strptime(Date, '%Y-%m-%d').strftime(Format_Date)
     
     def Truncate_Timestamp(self):
-        #Note Removing the timestamp from "Date"
-        self.Dataframe["Date"] = self.Dataframe["Date"].str[:10]
+        #Note Removing the timestamp from self.Date_Column
+        self.Dataframe[self.Date_Column] = self.Dataframe[self.Date_Column].str[:10]
 
     def Sum_Grouped_ETH(self):
-        #Note Group by "Date" then sum their "ETH"
-        self.Dataframe = self.Dataframe.groupby("Date").sum('ETH')
+        #Note Group by self.Date_Column then sum their "ETH"
+        self.Dataframe = self.Dataframe.groupby(self.Date_Column).sum('ETH')
 
     def Reset_Dataframe_Index(self):
-        #Note Make "Date" a column
+        #Note Make self.Date_Column a column
         self.Dataframe = self.Dataframe.reset_index(level=0)
 
     def Seperate_Date_Into_Lists(self, Day_Format, Month_Format, Year_Format):
@@ -60,7 +62,7 @@ class Graph_Factory(ABC):
         self.Formatted_Year_List = []
         self.Formatted_Year_and_Month_List = []
 
-        for Current_Date in self.Dataframe["Date"].tolist():
+        for Current_Date in self.Dataframe[self.Date_Column].tolist():
 
             #Note Get day of week day as an number (0-6). 0 being Sunday. 6 being Saturday
             Day = self.Strftime(Current_Date, Day_Format)
