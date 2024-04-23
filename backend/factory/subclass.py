690c86856c0268f4dac3e76a9c1ea24da19c1b63
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-23 19:37:29 -0400

    Deleted heatmap class

--- backend/factory/subclass.py
+++ backend/factory/subclass.py
@@ -2,7 +2,6 @@
 import backend.dataframe.transaction as Transaction_Dataframe, backend.graph.transaction as Transaction_Plotly
 from backend.factory.superclass import Graph_Factory
 import backend.graph.scatter as scatter_graph
-import backend.dataframe.heatmap as heatmap_dataframe, backend.graph.heatmap as heatmap_graph
 class Transaction(Graph_Factory):
     
     def __init__(self, Graph_Name, Tool):
@@ -26,21 +25,3 @@ class Scatter(Graph_Factory):
 
     def Create_Plotly(self):
         self.plotly_graph = scatter_graph.create_scatter_graph(self.Dataframe)
-
-class Heatmap(Graph_Factory):
-    
-    def __init__(self, Graph_Name, Tool):
-        super().__init__(Graph_Name, Tool)
-
-    def create_DataFrame(self):
-        super().create_DataFrame()
-        self.Truncate_Timestamp()
-        self.Sum_Grouped_ETH()
-        self.Reset_Dataframe_Index()
-        self.Seperate_Date_Into_Lists("%w", "%m", "%Y")
-        self.Insert_Date_Lists_into_Dataframe()
-        self.Dataframe = self.Group_By_and_Sum([self.Year_Column, self.Month_Number_Column, self.Weekday_Number_Column, self.Month_Year_Column], False)
-        self.axes_dictionary = heatmap_dataframe.create_heatmap_DataFrame(self.Dataframe)
-
-    def Create_Plotly(self):
-        self.plotly_graph = heatmap_graph.Create_Heatmap_Graph(self.axes_dictionary)
