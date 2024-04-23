495c7ae6580163429da2d1869113753d984c7c1a
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-23 19:32:34 -0400

    Deleted sunburst class

--- backend/factory/subclass.py
+++ backend/factory/subclass.py
@@ -3,7 +3,6 @@ import backend.dataframe.transaction as Transaction_Dataframe, backend.graph.tra
 from backend.factory.superclass import Graph_Factory
 import backend.graph.scatter as scatter_graph
 import backend.dataframe.heatmap as heatmap_dataframe, backend.graph.heatmap as heatmap_graph
-import backend.dataframe.sunburst as sunburst_dataframe, backend.graph.sunburst as sunburst_graph
 import plotly.express as plotlyX
 class Transaction(Graph_Factory):
     
@@ -104,22 +103,3 @@ class Heatmap(Graph_Factory):
 
     def Create_Plotly(self):
         self.plotly_graph = heatmap_graph.Create_Heatmap_Graph(self.axes_dictionary)
-
-class Sunburst(Graph_Factory):
-    
-    def __init__(self, Graph_Name, Tool):
-        super().__init__(Graph_Name, Tool)
-
-    def create_DataFrame(self):
-        super().create_DataFrame()
-        self.Truncate_Timestamp()
-        self.Sum_Grouped_ETH()
-        self.Reset_Dataframe_Index()
-        self.Seperate_Date_Into_Lists("%w", "%m", "%Y")
-        self.Insert_Date_Lists_into_Dataframe()
-        Year_Dataframe = self.Group_By_and_Sum(self.Year_Column)
-        Month_Year_Dataframe = self.Group_By_and_Sum(self.Month_Year_Column)
-        self.Dataframe = sunburst_dataframe.create_sunburst_DataFrame(Year_Dataframe, Month_Year_Dataframe, self.Dataframe)
-
-    def Create_Plotly(self):
-        self.plotly_graph = sunburst_graph.Create_Sunburst_Graph(self.Dataframe)
