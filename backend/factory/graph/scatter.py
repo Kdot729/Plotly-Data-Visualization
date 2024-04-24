09a47f6f405c70839c1022449e47b4e2153ea5c3
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 15:55:42 -0400

    Deleted function

--- backend/factory/graph/scatter.py
+++ backend/factory/graph/scatter.py
@@ -6,9 +6,6 @@ class Scatter(Graph_Factory):
     def __init__(self, Tool, Dataframe):
         super().__init__(Tool)
         self.Dataframe = Dataframe
-        
-    def create_DataFrame(self):
-        super().create_DataFrame()
 
     def Create_Plotly(self):
         self.plotly_graph = scatter_graph.create_scatter_graph(self.Dataframe)
