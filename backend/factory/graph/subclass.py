531be2e7445f775fd701d287a040f2c368b1ee9c
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 15:16:27 -0400

    Deleted scatter class code

--- backend/factory/graph/subclass.py
+++ backend/factory/graph/subclass.py
@@ -1,7 +1,6 @@
 
 import backend.dataframe.transaction as Transaction_Dataframe, backend.graph.transaction as Transaction_Plotly
 from backend.factory.graph.superclass import Graph_Factory
-import backend.graph.scatter as scatter_graph
 class Transaction(Graph_Factory):
     
     def __init__(self, Tool):
@@ -13,15 +12,3 @@ class Transaction(Graph_Factory):
 
     def Create_Plotly(self):
         self.plotly_graph = Transaction_Plotly.Create_Transaction_Graph(self.Dataframe, self.columns_name["Inequality Column"])
-
-
-class Scatter(Graph_Factory):
-
-    def __init__(self, Tool):
-        super().__init__(Tool)
-        
-    def create_DataFrame(self):
-        super().create_DataFrame()
-
-    def Create_Plotly(self):
-        self.plotly_graph = scatter_graph.create_scatter_graph(self.Dataframe)
