bae18c710217ec7861ee6485003c045937bc218a
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 17:15:59 -0400

    Moved Create_Heatmap_Dataframe into Heatmap class

--- backend/factory/dataframe/heatmap.py
+++ backend/factory/dataframe/heatmap.py
@@ -1,6 +1,6 @@
 
 from backend.factory.dataframe.superclass import Dataframe
-import backend.dataframe.heatmap as heatmap_dataframe
+import pandas as panda
 
 class Heatmap(Dataframe):
 
@@ -15,4 +15,38 @@ class Heatmap(Dataframe):
         self.Seperate_Date_Into_Lists("%w", "%m", "%Y")
         self.Insert_Date_Lists_into_Dataframe()
         self._Dataframe = self.Group_By_and_Sum([self.Year_Column, self.Month_Number_Column, self.Weekday_Number_Column, self.Month_Year_Column], False)
-        self.axes_dictionary = heatmap_dataframe.create_heatmap_DataFrame(self._Dataframe)
+        self.axes_dictionary = self.Create_Heatmap_Dataframe()
+
+    def Create_Heatmap_Dataframe(self):
+
+        #Note Get the list of "Month Year" to use to reorder DataFrame columns
+        columns_list = self._Dataframe["Month Year"].drop_duplicates().tolist()
+
+        self._Dataframe = panda.pivot_table(self._Dataframe, 
+                                        values='ETH', 
+                                        index=["Weekday Number"],
+                                        columns=["Month Year"], 
+
+                                        #Note If a weekday didn't have any volumne use a zero
+                                        fill_value=0)
+
+
+        #Note Changing the column order so Oct-2021 is first
+        self._Dataframe = self._Dataframe[columns_list]
+
+        #Note y_axis is the weekday as a number
+        #Delete? This is variable is in the graph.py
+        weekday_names_list =["Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"]
+
+
+
+        z_axis = []
+
+        for index, row in self._Dataframe.iterrows():
+                #Note Get all the values of a row and convert it to a list
+                row_list = row.values.tolist()
+
+                #Note Put the row_list into z_axis to make it a 2D array
+                z_axis.append(row_list)
+
+        return {"x": columns_list,"y": weekday_names_list, "z":z_axis}
