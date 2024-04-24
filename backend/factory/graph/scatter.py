80ad87f32edf0b25d28d42b35cf2269f78c6bb4b
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 15:58:18 -0400

    Moved scatter graph code to class function

--- backend/factory/graph/scatter.py
+++ backend/factory/graph/scatter.py
@@ -1,5 +1,5 @@
 from backend.factory.graph.superclass import Graph_Factory
-import backend.graph.scatter as scatter_graph
+import plotly.express as plotlyX
 
 class Scatter(Graph_Factory):
 
@@ -8,4 +8,28 @@ class Scatter(Graph_Factory):
         self.Dataframe = Dataframe
 
     def Create_Plotly(self):
-        self.plotly_graph = scatter_graph.create_scatter_graph(self.Dataframe)
+            
+        self.plotly_graph = plotlyX.scatter(self.Dataframe,
+                            x ='Date',
+                            y='ETH',
+                            # text=Type,
+                            # color_discrete_sequence=f"rgb(0,0,{viridis(1)})",
+                            hover_data=['Hash'],
+                            title="Transactions",
+
+                            #FIXME Testing width and height
+                            #! Delete later
+                            width=1600, height=630)
+                            # symbol=Type) #! Not sure if symbol is necessary
+
+        #! Uncomment below?
+        # figure.update_traces(textposition='top center')
+        # if Type == "Buyer":             #Note Made the graph green to represent people buying. Note sure if necessary
+        #     figure.update_traces(marker=dict(
+        #     color="green"
+        #     ))
+        # elif Type == "Seller":          #Note Made the graph red to represent people selling. Note sure if necessary
+        #     figure.update_traces(marker=dict(
+        #     color='red'))
+
+        self.plotly_graph.update_layout(title_x=0.5)
