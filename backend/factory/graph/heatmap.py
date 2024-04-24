bb57caea31f73081190bd24bd6b9b507724705f9
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 16:17:50 -0400

    Removed create_DataFrame. Changed constructor

--- backend/factory/graph/heatmap.py
+++ backend/factory/graph/heatmap.py
@@ -1,20 +1,11 @@
 from backend.factory.graph.superclass import Graph_Factory
-import backend.dataframe.heatmap as heatmap_dataframe, backend.graph.heatmap as heatmap_graph
+import backend.graph.heatmap as heatmap_graph
 
 class Heatmap(Graph_Factory):
     
-    def __init__(self, Tool):
+    def __init__(self, Tool, Dataframe_Object):
         super().__init__(Tool)
-
-    def create_DataFrame(self):
-        super().create_DataFrame()
-        self.Truncate_Timestamp()
-        self.Sum_Grouped_ETH()
-        self.Reset_Dataframe_Index()
-        self.Seperate_Date_Into_Lists("%w", "%m", "%Y")
-        self.Insert_Date_Lists_into_Dataframe()
-        self.Dataframe = self.Group_By_and_Sum([self.Year_Column, self.Month_Number_Column, self.Weekday_Number_Column, self.Month_Year_Column], False)
-        self.axes_dictionary = heatmap_dataframe.create_heatmap_DataFrame(self.Dataframe)
+        self.Dataframe_Object = Dataframe_Object
 
     def Create_Plotly(self):
-        self.plotly_graph = heatmap_graph.Create_Heatmap_Graph(self.axes_dictionary)
+        self.plotly_graph = heatmap_graph.Create_Heatmap_Graph(self.Dataframe_Object.axes_dictionary)
