4f050d6a52ab0b1d31e90f1ec6f1ce01e62bcce6
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 09:35:50 -0400

    Added getter. Changed variable to have _

--- backend/class/dataframe/superclass.py
+++ backend/class/dataframe/superclass.py
@@ -14,23 +14,27 @@ class Dataframe(ABC):
     def __init__(self, Tool):
         self.Tool = Tool
 
+    @property
+    def Dataframe(self): 
+        return self._Dataframe
+    
     def Create_Dataframe(self):
-        self.Dataframe = panda.read_csv(f"csv/{self.Tool}.csv", names=(self.Date_Column, 'Hash', self.ETH_Column, 'Seller', 'Buyer'))
+        self._Dataframe = panda.read_csv(f"csv/{self.Tool}.csv", names=(self.Date_Column, 'Hash', self.ETH_Column, 'Seller', 'Buyer'))
 
     def Strftime(self, Date, Format_Date):
         return datetime.strptime(Date, '%Y-%m-%d').strftime(Format_Date)
     
     def Truncate_Timestamp(self):
         #Note Removing the timestamp from self.Date_Column
-        self.Dataframe[self.Date_Column] = self.Dataframe[self.Date_Column].str[:10]
+        self._Dataframe[self.Date_Column] = self._Dataframe[self.Date_Column].str[:10]
 
     def Sum_Grouped_ETH(self):
         #Note Group by self.Date_Column then sum their "ETH"
-        self.Dataframe = self.Dataframe.groupby(self.Date_Column).sum(self.ETH_Column)
+        self._Dataframe = self._Dataframe.groupby(self.Date_Column).sum(self.ETH_Column)
 
     def Reset_Dataframe_Index(self):
         #Note Make self.Date_Column a column
-        self.Dataframe = self.Dataframe.reset_index(level=0)
+        self._Dataframe = self._Dataframe.reset_index(level=0)
 
     def Seperate_Date_Into_Lists(self, Day_Format, Month_Format, Year_Format):
 
@@ -39,7 +43,7 @@ class Dataframe(ABC):
         self.Formatted_Year_List = []
         self.Formatted_Year_and_Month_List = []
 
-        for Current_Date in self.Dataframe[self.Date_Column].tolist():
+        for Current_Date in self._Dataframe[self.Date_Column].tolist():
 
             #Note Get day of week day as an number (0-6). 0 being Sunday. 6 being Saturday
             Day = self.Strftime(Current_Date, Day_Format)
@@ -61,10 +65,10 @@ class Dataframe(ABC):
 
     #Note Only heatmap and sunburst use this function. Volume will override this function
     def Insert_Date_Lists_into_Dataframe(self):
-        self.Dataframe.insert(2, self.Year_Column, self.Formatted_Year_List, True)
-        self.Dataframe.insert(3, self.Month_Number_Column, self.Formatted_Month_List, True)
-        self.Dataframe.insert(4, self.Weekday_Number_Column, self.Formatted_Day_List, True)
-        self.Dataframe.insert(5, self.Month_Year_Column, self.Formatted_Year_and_Month_List, True)
+        self._Dataframe.insert(2, self.Year_Column, self.Formatted_Year_List, True)
+        self._Dataframe.insert(3, self.Month_Number_Column, self.Formatted_Month_List, True)
+        self._Dataframe.insert(4, self.Weekday_Number_Column, self.Formatted_Day_List, True)
+        self._Dataframe.insert(5, self.Month_Year_Column, self.Formatted_Year_and_Month_List, True)
     
     def Group_By_and_Sum(self, Group_By_Columns, Boolean_Sort=True):
-        return self.Dataframe.groupby(Group_By_Columns, sort=Boolean_Sort).sum(self.ETH_Column).reset_index()
+        return self._Dataframe.groupby(Group_By_Columns, sort=Boolean_Sort).sum(self.ETH_Column).reset_index()
