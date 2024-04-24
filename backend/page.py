80526c3990eb869cc7bb72805bf4aec93cd997ca
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-24 15:55:17 -0400

    Not calling function
    Function was moved to a different class so we're calling it in the match

--- backend/page.py
+++ backend/page.py
@@ -3,7 +3,6 @@ from backend.factory.graph.superclass import Graph_Factory
 def Render_Graph(Graph_Name, Tool):
     
     Chosen_Graph = Graph_Factory.Choose_Graph(Graph_Name, Tool)
-    Chosen_Graph.create_DataFrame()
     Chosen_Graph.Create_Plotly()
     Chosen_Graph.Convert_Plotly_to_JSON()
     return Chosen_Graph.Render_Graph()
