2949635885836ec5528136188d9e2660ec993a2e
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-25 12:01:41 -0400

    Putting everything in one update_layout call

--- backend/factory/graph/volume.py
+++ backend/factory/graph/volume.py
@@ -13,34 +13,34 @@ class Volume(Graph_Factory):
         y_axis = (self.Dataframe_Object._Dataframe[self.Dataframe_Object.Month_Year_Column].tolist())[::-1]
 
         self.Plotly_Graph = plotlyX.bar(self.Dataframe_Object._Dataframe, 
-                        x=self.Dataframe_Object.ETH_Column, 
-                        y=self.Dataframe_Object.Month_Year_Column, 
-                        color=self.Dataframe_Object.Day_Column, 
-                        orientation='h',
-                        category_orders={
-                                #Note Reorder the horizontal bars so "Monday" is first and "Sunday" is last
-                                self.Dataframe_Object.Day_Column: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                                        x=self.Dataframe_Object.ETH_Column, 
+                                        y=self.Dataframe_Object.Month_Year_Column, 
+                                        color=self.Dataframe_Object.Day_Column, 
+                                        orientation='h',
+                                        category_orders={
+                                                            #Note Reorder the horizontal bars so "Monday" is first and "Sunday" is last
+                                                            self.Dataframe_Object.Day_Column: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
 
-                                #Note This is to fix the y-axis because "February" is bugged
-                                self.Dataframe_Object.Month_Year_Column: y_axis
-                                })
+                                                            #Note This is to fix the y-axis because "February" is bugged
+                                                            self.Dataframe_Object.Month_Year_Column: y_axis
+                                                        }
+                                        )
 
-        #Note Create title for graph and center it
-        self.Plotly_Graph.update_layout(title_text="Volume", title_x=0.5)
+        self.Plotly_Graph.update_layout(width=self.Graph_Width, height=self.Graph_Height,
+                                        
+                                        #Note Create title for graph and center it
+                                        title_text="Volume", title_x=0.5,
+                                        
+                                        legend={
+                                                    #Note Change position of legend to be inside the graph
+                                                    "yanchor": "top",
+                                                    "y": 1,
+                                                    "xanchor":"right",
+                                                    "x": 1,
 
-        self.Plotly_Graph.update_layout(legend={
-                        #Note Change position of legend to be inside the graph
-                        "yanchor": "top",
-                        "y": 1,
-                        "xanchor":"right",
-                        "x": 1,
-
-                        #Note Styling the legend box
-                        "bgcolor": "#e5ecf6",
-                        "bordercolor": "black",
-                        "borderwidth": 1
-                        })
-        self.Plotly_Graph.update_layout(width=1580, 
-                            height=750)
-
-        self.Plotly_Graph
+                                                    #Note Styling the legend box
+                                                    "bgcolor": "#e5ecf6",
+                                                    "bordercolor": "black",
+                                                    "borderwidth": 1
+                                                }
+                                        )
