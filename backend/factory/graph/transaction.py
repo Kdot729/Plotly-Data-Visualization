43d17a02b202af699107235b999ca07857be5cda
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-25 10:12:24 -0400

    Moved graph code into the class

--- backend/factory/graph/transaction.py
+++ backend/factory/graph/transaction.py
@@ -1,10 +1,18 @@
 
-import backend.factory.dataframe.transaction as Transaction_Dataframe, backend.graph.transaction as Transaction_Plotly
 from backend.factory.graph.superclass import Graph_Factory
+import plotly.express as plotlyX
 class Transaction(Graph_Factory):
     
     def __init__(self, Tool, Dataframe_Object):
         super().__init__(Tool, Dataframe_Object)
 
     def Create_Plotly(self):
-        self.Plotly_Graph = Transaction_Plotly.Create_Transaction_Graph(self.Dataframe, self.columns_name["Inequality Column"])
+
+        self.Plotly_Graph = plotlyX.bar(
+                                        self.Dataframe_Object._Dataframe, 
+                                        x="Address", 
+                                        y="Transaction Count",
+                                        title="Transactions Per Address"
+                                        )
+        self.Plotly_Graph.update_layout(title_x=0.5)
+        self.Plotly_Graph.update_xaxes(tickfont_size=7)
