964d3b65f34839d036e8c542c1211b9531352ebc
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-25 07:35:52 -0400

    Using class variables instead of local variables

--- backend/factory/dataframe/sunburst.py
+++ backend/factory/dataframe/sunburst.py
@@ -20,29 +20,23 @@ class Sunburst(Dataframe):
 
     def Create_Sunburst_Dataframe(self):
 
-        Year_Column = "Year"
-        Month_Year_Column = "Month Year"
-        Date_Column = "Date"
-        ETH_Column = "ETH"
-
         Lowercase_Total = "total"
-
         Column_Key = "Column"
         Dataframe_Key = "Dataframe"
 
         Graph_Dataframe = panda.DataFrame(columns=["ID", "Parent", "Value", "Text"])
 
         #Note Doesn't matter which dataframe we sum
-        Sum = self.Year_Dataframe[ETH_Column].sum()
+        Sum = self.Year_Dataframe[self.ETH_Column].sum()
 
         #Note This render the center of the sunburst
         #Important The ID (2nd index) has to be an empty string
         Graph_Dataframe.loc[0] = [Lowercase_Total, "", Sum, Lowercase_Total.upper()]
 
         Dataframe_Array =   [
-                                {Dataframe_Key: self.Year_Dataframe, Column_Key: Year_Column},
-                                {Dataframe_Key: self.Month_Year_Dataframe, Column_Key: Month_Year_Column},
-                                {Dataframe_Key: self.Dataframe, Column_Key: Date_Column}
+                                {Dataframe_Key: self.Year_Dataframe, Column_Key: self.Year_Column},
+                                {Dataframe_Key: self.Month_Year_Dataframe, Column_Key: self.Month_Year_Column},
+                                {Dataframe_Key: self.Dataframe, Column_Key: self.Date_Column}
                             ]
 
         for Dictionary in Dataframe_Array:
@@ -51,20 +45,20 @@ class Sunburst(Dataframe):
 
             for Index, Row in Dictionary[Dataframe_Key].iterrows():
 
-                ETH_Value = Row[ETH_Column]
+                ETH_Value = Row[self.ETH_Column]
                 Value = Row[Dataframe_Column]
 
-                if Dataframe_Column == Year_Column:
+                if Dataframe_Column == self.Year_Column:
 
                     Insert_Row = [Value, Lowercase_Total, ETH_Value, Value]
 
-                elif Dataframe_Column == Month_Year_Column:
+                elif Dataframe_Column == self.Month_Year_Column:
 
                     Abbreviated_Month = datetime.strptime(Value, '%Y-%m').strftime('%b')
                     #Note Slicing "Month Year" value
                     Insert_Row = [Value, Value[:4], ETH_Value, Abbreviated_Month]
 
-                elif Dataframe_Column == Date_Column:    
+                elif Dataframe_Column == self.Date_Column:    
 
                     Non_Zero_Padded_Day = datetime.strptime(Value, '%Y-%m-%d').strftime('%-d')
                     Nth_Day = f"{Non_Zero_Padded_Day}th"
