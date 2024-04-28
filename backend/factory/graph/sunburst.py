5c2ab03e9e6d314184593305679130438d57e074
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-04-28 08:34:47 -0400

    Changed color of sunburst

--- backend/factory/graph/sunburst.py
+++ backend/factory/graph/sunburst.py
@@ -1,7 +1,6 @@
 from backend.factory.graph.superclass import Graph_Factory
 import plotly.graph_objects as plotGO
 from plotly.subplots import make_subplots
-import numpy
 class Sunburst(Graph_Factory):
     
     def __init__(self, Tool, Dataframe_Object):
@@ -12,23 +11,20 @@ class Sunburst(Graph_Factory):
         self.Plotly_Graph = make_subplots(specs=[[{"type": "domain"}]])
 
         #Note Unpacking array
-        ID_Column, Parent_Column, Value_Column, Text_Column = self.Dataframe_Object.Dataframe.columns
+        ID_Column, Parent_Column, Value_Column, Text_Column, Color_Column = self.Dataframe_Object.Dataframe.columns
 
         Sunburst =  plotGO.Sunburst(
                                     labels=self.Dataframe_Object.Dataframe[ID_Column],
                                     parents=self.Dataframe_Object.Dataframe[Parent_Column],
                                     values=self.Dataframe_Object.Dataframe[Value_Column],
                                     branchvalues='total',
-
                                     insidetextorientation="radial",
-                                    # textinfo="label",
                                     texttemplate=self.Dataframe_Object.Dataframe[Text_Column],
-                                    
                                     hovertemplate='<b>%{label}</b><br> Volume: %{value} ETH',
 
                                     #Note name removes the extra box to the side when you hover
                                     name='',
-                                    # level=''
+                                    marker=dict(colors=self.Dataframe_Object.Dataframe[Color_Column])
                                     )
         
         self.Plotly_Graph.add_trace(Sunburst)
